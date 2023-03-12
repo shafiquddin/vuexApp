fbc184776b2b5f4d2f2254207a08af377b410a7f
shafiquddin <shafiquddin2k@gmail.com>
2023-03-13 01:05:06 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <the-header></the-header>
+  <router-view v-if="isLoggedIn"></router-view>
+</template>
+
+<script>
+import TheHeader from './components/nav/TheHeader.vue';
+
+export default {
+  components: {
+    TheHeader
+  },
+  computed:{
+    isLoggedIn(){
+    return this.$store.getters.isLoggedIn;
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,108 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      isLoggedIn: false,
+      cart: { items: [], total: 0, qty: 0 },
+      products: [
+        {
+          id: "p1",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Books_HD_%288314929977%29.jpg/640px-Books_HD_%288314929977%29.jpg",
+          title: "Book Collection",
+          description:
+            "A collection of must-read books. All-time classics included!",
+          price: 99.99,
+        },
+        {
+          id: "p2",
+          image:
+            "https://upload.wikimedia.org/wikipedia/en/thumb/c/c9/Tent_at_High_Shelf_Camp_cropped.jpg/640px-Tent_at_High_Shelf_Camp_cropped.jpg",
+          title: "Mountain Tent",
+          description: "A tent for the ambitious outdoor tourist.",
+          price: 129.99,
+        },
+        {
+          id: "p3",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg/640px-Good_Food_Display_-_NCI_Visuals_Online.jpg",
+          title: "Food Box",
+          description:
+            "May be partially expired when it arrives but at least it is cheap!",
+          price: 6.99,
+        },
+      ],
+    };
+  },
+  mutations:{
+    addProductToCart(state,productData) {
+        const productInCartIndex = state.cart.items.findIndex(
+          (ci) => ci.productId === productData.id
+        );
+  
+        if (productInCartIndex >= 0) {
+          state.cart.items[productInCartIndex].qty++;
+        } else {
+          const newItem = {
+            productId: productData.id,
+            title: productData.title,
+            image: productData.image,
+            price: productData.price,
+            qty: 1,
+          };
+          state.cart.items.push(newItem);
+        }
+        state.cart.qty++;
+        state.cart.total += productData.price;
+      },
+      removeProductFromCart(state,prodId) {
+        const productInCartIndex = state.cart.items.findIndex(
+          (cartItem) => cartItem.productId === prodId
+        );
+        const prodData = state.cart.items[productInCartIndex];
+        state.cart.items.splice(productInCartIndex, 1);
+        state.cart.qty -= prodData.qty;
+        state.cart.total -= prodData.price * prodData.qty;
+      },
+      login(state) {
+        state.isLoggedIn = true;
+      },
+      logout(state) {
+        state.isLoggedIn = false;
+      },
+      setAuth(state,payload){
+        state.isLoggedIn=payload.auth;
+      }
+  },
+  actions:{
+    addProductToCart(context,payload){
+        context.commit('addProductToCart',payload)
+    },
+    removeProductFromCart(context,payload){
+        context.commit('removeProductFromCart',payload)
+    },
+    setAuth(context,payload){
+        context.commit('setAuth',payload);
+    },
+    login(context){
+        context.commit('login')
+    },
+    logout(context){
+        context.commit('logout')
+    }
+  },
+  getters:{
+    products(state){
+        return state.products;
+    },
+    cart(state){
+        return state.cart;
+    },
+    isLoggedIn(state){
+        return state.isLoggedIn;
+    }
+  },
+});
+
+export default store;
